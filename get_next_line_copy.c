a09916bc982527f5de29da2e2ea6c5ce51147f37
Audran DITSCH <aditsch@e1r4p1.42.fr>
2016-11-20 17:06:30 +0100

    wip

--- get_next_line_copy.c
+++ get_next_line_copy.c
@@ -6,82 +6,57 @@
 /*   By: aditsch <aditsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 01:55:22 by aditsch           #+#    #+#             */
-/*   Updated: 2016/11/20 17:05:06 by aditsch          ###   ########.fr       */
+/*   Updated: 2016/11/20 13:58:28 by aditsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_fd		*ft_manage_fd(t_fd **list, int fd)
+void 		print_list_fd(t_fd **list)
+{
+	t_fd	*list_i;
+
+	list_i = *list;
+	if (list_i)
+	{
+		while (list_i)
+		{
+			printf("FD%d ", list_i->fd);
+			list_i = list_i->next;
+		}
+	}
+	printf("\n");
+}
+
+t_fd	*ft_fd_manager(t_fd **list, int fd)
 {
 	while (*list && (*list)->fd != fd)
 		list = &(*list)->next;
 	if (!*list)
 	{
 		*list = (t_fd *)malloc(sizeof(**list));
-		(*list)->tmp = NULL;
 		(*list)->fd = fd;
 		(*list)->next = NULL;
 	}
 	return (*list);
 }
 
-int			ft_read_tmp(char **line, t_fd *list)
-{
-	char	*tmp;
-
-	if (!list->tmp)
-		return (0);
-	if ((tmp = ft_strchr(list->tmp, '\n')))
-	{
-		*line = ft_strjoin(*line, ft_strsub(list->tmp, 0, tmp - list->tmp));
-		list->tmp = ft_strdup(list->tmp + (tmp - list->tmp) + 1);
-		return (1);
-	}
-	else
-		*line = ft_strjoin(*line, list->tmp);
-	return (0);
-}
-
-int			ft_read_fd(char **line, t_fd *list)
-{
-	int		count;
-	char 	buffer[BUFF_SIZE + 1];
-	char	*tmp;
-
-	while ((count = read(list->fd, buffer, BUFF_SIZE)))
-	{
-		if (count == -1)
-			return (-1);
-		buffer[count] = '\0';
-		if ((tmp = ft_strchr(buffer, '\n')))
-		{
-			*line = ft_strjoin(*line, ft_strsub(buffer, 0, tmp - buffer));
-			list->tmp = ft_strdup(buffer + (tmp - buffer) + 1);
-			return (1);
-		}
-		else
-			*line = ft_strjoin(*line, buffer);
-	}
-	if (count == 0 && **line == '\0')
-		return (0);
-	return (1);
-}
-
 int			get_next_line(int const fd, char **line)
 {
+	int				count;
+	char			buffer[BUFF_SIZE + 1];
 	static t_fd		*list = NULL;
+	int				i;
 	t_fd			*current_fd;
 
 	if (!line || fd == -1)
 		return (-1);
 	if (*line)
 		*line = NULL;
-	*line = ft_strdup("");
-	current_fd = ft_manage_fd(&list, fd);
-	if (ft_read_tmp(line, current_fd))
-		return (1);
-	return (ft_read_fd(line, current_fd));
+	current_fd = ft_fd_manager(&list, fd);
+	printf("current fd = %d - adress = %p\n", current_fd->fd, &(current_fd->fd));
+	print_list_fd(&list);
+	return (FALSE);
 }
 
 int			main(int argc, char *argv[])
@@ -95,23 +70,16 @@ int			main(int argc, char *argv[])
 	int 	fd_6 = open(argv[6],O_RDONLY);
 	char	*line = NULL;
 
+	printf(" close: %d ", close(fd_1));
+
 	get_next_line(fd_4, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_1, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_2, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_1, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_3, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_4, &line);
-	printf("line = %s\n", line);
-	get_next_line(fd_5, &line); // Test with not existing file
-	printf("line = %s\n", line);
+	get_next_line(fd_5, &line);		// Test with not existing file
 	get_next_line(fd_6, &line);
-	printf("line = %s\n", line);
 	get_next_line(fd_1, &line);
-	printf("line = %s\n", line);
 	return (FALSE);
 }
